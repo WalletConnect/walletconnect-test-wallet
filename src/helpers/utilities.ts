11b0f954c15bb4c0de282a3850b02532fd2c3159
Pedro Gomes <pedrogomes94@gmail.com>
2019-04-18 16:59:37 +0200

    getChainData

--- src/helpers/utilities.ts
+++ src/helpers/utilities.ts
@@ -109,31 +109,31 @@ export function payloadId(): number {
   return id;
 }
 
-export function getChainData (chainId: number): IChainData {
+export function getChainData(chainId: number): IChainData {
   const chainData = supportedChains.filter(
     (chain: any) => chain.chain_id === chainId
-  )[0]
+  )[0];
 
   if (!chainData) {
-    throw new Error('ChainId missing or not supported')
+    throw new Error("ChainId missing or not supported");
   }
 
-  const API_KEY = process.env.REACT_APP_INFURA_PROJECT_ID
+  const API_KEY = process.env.REACT_APP_INFURA_PROJECT_ID;
 
   if (
-    chainData.rpc_url.includes('infura.io') &&
-    chainData.rpc_url.includes('%API_KEY%') &&
+    chainData.rpc_url.includes("infura.io") &&
+    chainData.rpc_url.includes("%API_KEY%") &&
     API_KEY
   ) {
-    const rpcUrl = chainData.rpc_url.replace('%API_KEY%', API_KEY)
+    const rpcUrl = chainData.rpc_url.replace("%API_KEY%", API_KEY);
 
     return {
       ...chainData,
       rpc_url: rpcUrl
-    }
+    };
   }
 
-  return chainData
+  return chainData;
 }
 
 export function getViewportDimensions() {
