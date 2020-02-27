01b17d3076b1cfbf44665ae2b1e16f084c28ef43
Pedro Gomes <pedrogomes94@gmail.com>
2020-02-27 21:25:33 +0100

    add starkware engine

---
+++ src/config/rpcEngine/starkware.ts
@@ -0,0 +1,142 @@
+import { IRpcEngine } from "../../helpers/types";
+import {
+  starkwareMethods,
+  starkwareGetAccounts,
+  starkwareRegister,
+  starkwareDeposit,
+  starkwareSignTransfer,
+  starkwareSignCreateOrder,
+} from "../helpers/starkware";
+import { IAppState } from "src/App";
+
+function filterStarkwareRequests(payload: any) {
+  return starkwareMethods.includes(payload.method);
+}
+
+async function routeStarkwareRequests(payload: any, state: IAppState, setState: any) {
+  if (!state.connector) {
+    return;
+  }
+  if (payload.method === "stark_accounts") {
+    state.connector.approveRequest({
+      id: payload.id,
+      result: {
+        accounts: await starkwareGetAccounts(),
+      },
+    });
+  } else {
+    const requests = state.requests;
+    requests.push(payload);
+    await setState({ requests });
+  }
+}
+
+function renderStarkwareRequests(payload: any) {
+  // TODO: fix stark key
+  let params = [{ label: "StarkKey", value: "STARK_KEY_GOES_HERE" }];
+
+  switch (payload.method) {
+    case "stark_register":
+      params = [...params];
+      break;
+
+    case "stark_deposit":
+      params = [
+        ...params,
+        { label: "Token", value: payload.params.token },
+        { label: "Amount", value: payload.params.amount },
+      ];
+      break;
+    case "stark_transfer":
+      params = [
+        ...params,
+        { label: "amount", value: payload.params.amount },
+        { label: "nonce", value: payload.params.nonce },
+        { label: "senderVaultId", value: payload.params.senderVaultId },
+        { label: "token", value: payload.params.token },
+        { label: "receiverVaultId", value: payload.params.receiverVaultId },
+        { label: "receiverPublicKey", value: payload.params.receiverPublicKey },
+        { label: "expirationTimestamp", value: payload.params.expirationTimestamp },
+      ];
+      break;
+    default:
+      params = [
+        ...params,
+        { label: "vaultSell", value: payload.params.vaultSell },
+        { label: "vaultBuy", value: payload.params.vaultBuy },
+        { label: "amountSell", value: payload.params.amountSell },
+        { label: "amountBuy", value: payload.params.amountBuy },
+        { label: "tokenSell", value: payload.params.tokenSell },
+        { label: "tokenBuy", value: payload.params.tokenBuy },
+        { label: "nonce", value: payload.params.nonce },
+        { label: "expirationTimestamp", value: payload.params.expirationTimestamp },
+      ];
+      break;
+  }
+  return params;
+}
+
+async function signStarkwareRequests(payload: any, state: IAppState, setState: any) {
+  if (!state.connector) {
+    return;
+  }
+  const { connector } = state;
+  const { id, method, params } = payload;
+  switch (method) {
+    case "stark_register":
+      // TODO: Display register screen
+      connector.approveRequest({
+        id,
+        result: await starkwareRegister(),
+      });
+      break;
+    case "stark_deposit":
+      // TODO: Display deposit screen
+      connector.approveRequest({
+        id,
+        result: await starkwareDeposit(params.amount, params.token),
+      });
+      break;
+    case "stark_transfer":
+      // TODO: Display deposit screen
+      connector.approveRequest({
+        id,
+        result: await starkwareSignTransfer(
+          params.amount,
+          params.nonce,
+          params.senderVaultId,
+          params.token,
+          params.receiverVaultId,
+          params.receiverPublicKey,
+          params.expirationTimestamp,
+        ),
+      });
+      break;
+    case "stark_createOrder":
+      connector.approveRequest({
+        id,
+        result: await starkwareSignCreateOrder(
+          params.vaultSell,
+          params.vaultBuy,
+          params.amountSell,
+          params.amountBuy,
+          params.tokenSell,
+          params.tokenBuy,
+          params.nonce,
+          params.expirationTimestamp,
+        ),
+      });
+      break;
+    default:
+      throw new Error(`Unknown Starkware RPC Method: ${method}`);
+  }
+}
+
+const starkware: IRpcEngine = {
+  filter: filterStarkwareRequests,
+  router: routeStarkwareRequests,
+  render: renderStarkwareRequests,
+  signer: signStarkwareRequests,
+};
+
+export default starkware;
