c59f2405a2edf6095e99dbe48057e4eaf0770d06
Pedro Gomes <pedrogomes94@gmail.com>
2020-04-04 18:42:04 +0200

    add controllers

---
+++ src/controllers/wallet.ts
@@ -0,0 +1,171 @@
+import * as ethers from "ethers";
+import { getChainData } from "../helpers/utilities";
+import { setLocal, getLocal } from "../helpers/local";
+import {
+  ENTROPY_KEY,
+  MNEMONIC_KEY,
+  DEFAULT_ACTIVE_INDEX,
+  DEFAULT_CHAIN_ID,
+} from "../helpers/constants";
+import appConfig from "../config";
+
+export class WalletController {
+  public path: string | null = null;
+  public entropy: string | null = null;
+  public mnemonic: string | null = null;
+  public activeIndex: number = DEFAULT_ACTIVE_INDEX;
+  public activeChainId: number = DEFAULT_CHAIN_ID;
+  public wallet: ethers.Wallet | null = null;
+
+  public isActive() {
+    if (!this.wallet) {
+      return this.wallet;
+    }
+    return null;
+  }
+
+  public getIndex() {
+    return this.activeIndex;
+  }
+
+  public getWallet(index?: number, chainId?: number): ethers.Wallet {
+    if (!this.wallet || this.activeIndex === index || this.activeChainId === chainId) {
+      return this.init(index, chainId);
+    }
+    return this.wallet;
+  }
+
+  public getAccounts(count = appConfig.numberOfAccounts) {
+    const accounts = [];
+    let wallet = null;
+    for (let i = 0; i < count; i++) {
+      wallet = this.generateWallet(i);
+      accounts.push(wallet.address);
+    }
+    return accounts;
+  }
+
+  public getData(key: string): string {
+    let value = getLocal(key);
+    if (!value) {
+      switch (key) {
+        case ENTROPY_KEY:
+          value = this.generateEntropy();
+          break;
+        case MNEMONIC_KEY:
+          value = this.generateMnemonic();
+          break;
+        default:
+          throw new Error(`Unknown data key: ${key}`);
+      }
+      setLocal(key, value);
+    }
+    return value;
+  }
+
+  public generatePath(index: number) {
+    this.path = `${appConfig.derivationPath}/${index}`;
+    return this.path;
+  }
+
+  public generateEntropy(): string {
+    this.entropy = ethers.utils.hexlify(ethers.utils.randomBytes(16));
+    return this.entropy;
+  }
+
+  public generateMnemonic() {
+    this.mnemonic = ethers.utils.HDNode.entropyToMnemonic(this.getEntropy());
+    return this.mnemonic;
+  }
+
+  public generateWallet(index: number) {
+    this.wallet = ethers.Wallet.fromMnemonic(this.getMnemonic(), this.generatePath(index));
+    return this.wallet;
+  }
+
+  public getEntropy(): string {
+    return this.getData(ENTROPY_KEY);
+  }
+
+  public getMnemonic(): string {
+    return this.getData(MNEMONIC_KEY);
+  }
+
+  public init(index = DEFAULT_ACTIVE_INDEX, chainId = DEFAULT_CHAIN_ID) {
+    return this.update(index, chainId);
+  }
+
+  public update(index: number, chainId: number) {
+    this.activeIndex = index;
+    this.activeChainId = chainId;
+    const rpcUrl = getChainData(chainId).rpc_url;
+    this.wallet = this.generateWallet(index);
+    const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+    this.wallet.connect(provider);
+    return this.wallet;
+  }
+
+  public async sendTransaction(transaction: any) {
+    if (this.wallet) {
+      if (
+        transaction.from &&
+        transaction.from.toLowerCase() !== this.wallet.address.toLowerCase()
+      ) {
+        console.error("Transaction request From doesn't match active account");
+      }
+
+      if (transaction.from) {
+        delete transaction.from;
+      }
+
+      // ethers.js expects gasLimit instead
+      if ("gas" in transaction) {
+        transaction.gasLimit = transaction.gas;
+        delete transaction.gas;
+      }
+
+      const result = await this.wallet.sendTransaction(transaction);
+      return result.hash;
+    } else {
+      console.error("No Active Account");
+    }
+    return null;
+  }
+
+  public async signTransaction(data: any) {
+    if (this.wallet) {
+      if (data && data.from) {
+        delete data.from;
+      }
+      const result = await this.wallet.sign(data);
+      return result;
+    } else {
+      console.error("No Active Account");
+    }
+    return null;
+  }
+
+  public async signMessage(data: any) {
+    if (this.wallet) {
+      const signingKey = new ethers.utils.SigningKey(this.wallet.privateKey);
+      const sigParams = await signingKey.signDigest(ethers.utils.arrayify(data));
+      const result = await ethers.utils.joinSignature(sigParams);
+      return result;
+    } else {
+      console.error("No Active Account");
+    }
+    return null;
+  }
+
+  public async signPersonalMessage(message: any) {
+    if (this.wallet) {
+      const result = await this.wallet.signMessage(
+        ethers.utils.isHexString(message) ? ethers.utils.arrayify(message) : message,
+      );
+      return result;
+    } else {
+      console.error("No Active Account");
+    }
+    return null;
+  }
+}
