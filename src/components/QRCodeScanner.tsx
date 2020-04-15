240c50965efd2bd851e512dbf737f4a544eb2b89
Pedro Gomes <pedrogomes94@gmail.com>
2020-04-15 16:12:35 +0200

    tweak

--- src/components/QRCodeScanner.tsx
+++ src/components/QRCodeScanner.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import styled from "styled-components";
-import * as QrReader from "react-qr-reader";
+import QrReader from "react-qr-reader";
 
 const SQRCodeScannerContainer = styled.div`
   position: fixed;
