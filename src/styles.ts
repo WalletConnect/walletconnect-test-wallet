fda3b5872c75ecaaebece7363715d26c91262608
Pedro Gomes <pedrogomes94@gmail.com>
2020-07-10 22:37:46 +0200

    fix styles

--- src/styles.ts
+++ src/styles.ts
@@ -90,8 +90,6 @@ export const responsive = {
 };
 
 export const globalStyle = `
-  @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,500,600,700,800');
-
   html, body, #root {
     height: 100%;
     width: 100%;
