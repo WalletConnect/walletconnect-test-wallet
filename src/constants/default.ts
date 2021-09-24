2c1a732e8359bc7adf47647d8f99e69daf2997fa
amiecorso <amie@nori.com>
2021-09-24 14:45:39 -0700

    set MNEMONIC via env var

--- src/constants/default.ts
+++ src/constants/default.ts
@@ -1,6 +1,6 @@
 export const ETH_STANDARD_PATH = "m/44'/60'/0'/0";
 export const ENTROPY_KEY = "ENTROPY";
-export const MNEMONIC_KEY = "MNEMONIC";
+export const MNEMONIC_KEY = process.env.REACT_APP_MNEMONIC_KEY as string;
 
 export const DEFAULT_ACTIVE_INDEX = 0;
 export const MAINNET_CHAIN_ID = 1;
